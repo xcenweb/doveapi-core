96b0ddc30e5fc799a931894a84862cb9bae591e1
一只咕鸽 <xcenweb@qq.com>
2022-10-19 00:15:22 +0800

    细节修改

--- src/DoveAPI.php
+++ src/DoveAPI.php
@@ -11,18 +11,18 @@
 use dove\Debug;
 use dove\Config;
 
-define('DOVE_VERSION','1.1.2');
-define('DOVE_START_TIME',microtime(true));
+define('DOVE_VERSION', '1.1.2');
+define('DOVE_START_TIME', microtime(true));
 
-define('ROOT_DIR',str_replace(['\\','//'],'/',dirname(dirname(dirname(dirname(__DIR__))))).'/');
+define('ROOT_DIR', str_replace(['\\','//'],'/',dirname(dirname(dirname(dirname(__DIR__))))).'/');
 
-define('DOVE_DIR',__DIR__.'/');
-define('DOVE_APP_DIR',ROOT_DIR.'app/');        // 应用目录
-define('DOVE_CONFIG_DIR',ROOT_DIR.'config/');  // 配置目录
-define('DOVE_DATA_DIR',ROOT_DIR.'data/');      // 数据目录
-define('DOVE_EXTEND_DIR',ROOT_DIR.'extend/');  // 扩展目录
-define('DOVE_PUBLIC_DIR',ROOT_DIR.'public/');  // 公共访问目录
-define('DOVE_RUNTIME_DIR',ROOT_DIR.'runtime/');// 运行目录
+define('DOVE_DIR', __DIR__.'/');
+define('DOVE_APP_DIR', ROOT_DIR.'app/');        // 应用目录
+define('DOVE_CONFIG_DIR', ROOT_DIR.'config/');  // 配置目录
+define('DOVE_DATA_DIR', ROOT_DIR.'data/');      // 数据目录
+define('DOVE_EXTEND_DIR', ROOT_DIR.'extend/');  // 扩展目录
+define('DOVE_PUBLIC_DIR', ROOT_DIR.'public/');  // 公共访问目录
+define('DOVE_RUNTIME_DIR', ROOT_DIR.'runtime/');// 运行目录
 
 Debug::register();
 Config::get('dove','debug')?error_reporting(E_ALL):error_reporting(0);
